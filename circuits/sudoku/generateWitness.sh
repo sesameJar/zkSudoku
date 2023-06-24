8b3cf030baec912d6100d9b179cd84fef0205596
Mehrad Kavian <mehrad.kavian@gmail.com>
2023-06-24 11:57:21 -0400

    added inputs and witness generator

--- circuits/sudoku/generateWitness.sh
+++ circuits/sudoku/generateWitness.sh
@@ -10,3 +10,6 @@ fi
 
 # Compile the circuit
 circom ${CIRCUIT}.circom --r1cs --wasm --sym --c
+
+# Generate the witness.wtns
+node ${CIRCUIT}_js/generate_witness.js ${CIRCUIT}_js/${CIRCUIT}.wasm input.json ${CIRCUIT}_js/witness.wtns
